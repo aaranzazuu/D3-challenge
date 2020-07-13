dd3c5b0ff21d149f39eab7c4f2a9387690b635be
Aranzazu Ballesteros <aaranzazuu@gmail.com>
2020-07-12 17:41:24 -0700

    lists of variables

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,23 @@
 // @TODO: YOUR CODE HERE!
+
+abbr = []
+healthcare = []
+smokes = []
+poverty = []
+age = []
+// Retrieve data from the CSV file and execute everything below
+d3.csv("../assets/data/data.csv").then(function(healthData) {
+    healthData.forEach(function(data){
+        abbr.push(data.abbr)
+        poverty.push(data.poverty)
+        healthcare.push(data.healthcare)
+        smokes.push(data.smokes)
+        age.push(data.age)
+    })
+    console.log(abbr)
+    console.log(healthcare)
+    console.log(smokes)
+    console.log(poverty)
+    console.log(age)
+        }
+        );
